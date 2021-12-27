fc4404802f8013af1f6afe3e92b9711d49e34027
Hykilpikonna <me@hydev.org>
2021-12-27 11:41:35 -0500

    [+] Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION=$(python3 -c 'import hypy_utils; print(hypy_utils.__version__)')
+echo "$VERSION"
+
+# Build
+python3 setup.py sdist bdist_wheel
+
+# Check
+python3 -m twine check dist/*
+
+# Upload
+python3 -m twine upload dist/*
