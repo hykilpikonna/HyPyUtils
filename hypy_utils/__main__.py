6c33a0388686fbf1669c43640ccd0b7867f11128
Hykilpikonna <me@hydev.org>
2021-12-27 11:28:33 -0500

    [+] Create setup.py

---
+++ hypy_utils/__main__.py
@@ -0,0 +1,7 @@
+
+def main():
+    print('🐱')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,36 @@
+import pathlib
+import hypy_utils
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="hypy_utils",
+    version=hypy_utils.__version__,
+    description="Python utility functions for HyDEV",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hykilpikonna/HyPyUtils",
+    author="Azalea Gui",
+    author_email="me@hydev.org",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "hypy_utils=hypy_utils.__main__:main",
+        ]
+    },
+)
